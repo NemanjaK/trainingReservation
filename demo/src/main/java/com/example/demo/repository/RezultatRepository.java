2b7730b245917b14a47a03af1f8af7ece25384f8
Nemanja Kovačević <kovacevicnemanja97@gmail.com>
2022-07-24 23:12:08 +0200

    back

--- demo/src/main/java/com/example/demo/repository/RezultatRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Rezultat;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RezultatRepository extends JpaRepository<Rezultat, Long> {
-}
